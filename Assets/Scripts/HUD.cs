3f0f779d88d1e8ad877b06462723252046623afc
Cesar palome <A01746493@itesm.mx>
2022-04-21 10:53:34 -0500

    HUD
    Creamos el menu de pausa

---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HUD : MonoBehaviour
+{
+
+    public static HUD instance;
+
+
+    private void Awake()
+    {
+        instance = this;
+
+    }
+
+    public void Salir()
+    {
+        SceneManager.LoadScene("Inicio");
+        // Investigar como descargar la escena actual
+    }
+
+    public void Setting()
+    {
+        SceneManager.LoadScene("Setting");
+        // Investigar como descargar la escena actual
+    }
+}
---
+++ Assets/Scripts/MenuPausa.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+     * Autor: Cesar Emiliano Palome Luna
+     * Funcion para pausar
+     */
+
+public class MenuPausa : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject panelPausa;
+    public bool estaPausado = false;
+
+    public static MenuPausa instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+
+
+
+    public void Pausar()
+    {
+        estaPausado = !estaPausado;
+        panelPausa.SetActive(estaPausado);
+        Time.timeScale = estaPausado ? 0 : 1;
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pausar();
+        }
+    }
+
+}
+   
+
+  
+
