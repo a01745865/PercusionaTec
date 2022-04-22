7f7934e9313a86e5ec978c5d19f5a309650045da
A01745419 <69483112+A01745419@users.noreply.github.com>
2022-04-21 19:45:11 -0500

    C# registrarse (aun no funciona)
    Como no podemos usar php, habria q hacer la api de marciano :v

---
+++ Assets/Scripts/RegistrarDatos.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Networking;
+// Autores: Jose Luis Madrigal, Erika Marlene Garcia, Cesar Emiliano Palome, Jose Angel Garcia
+// Clase para hacer conexion web con unity
+
+public class RegistrarDatos : MonoBehaviour
+{
+    public TextMeshProUGUI resultado;
+
+    //Los datos de entrada
+    public TMP_InputField textoNombre;
+    public TMP_InputField textoCiudad;
+    public TMP_InputField textoMail;
+    public TMP_InputField textoContrasena;
+    public TMP_InputField textoFechaNaci;
+    public TMP_InputField textoNacionalidad;
+
+    //Enviar los datos al servidor(click del boton)
+    public void EnviarDatos()
+    {
+        StartCoroutine(SubirDatos());
+    }
+
+    private IEnumerator SubirDatos()
+    {
+        //Recuperar los datos
+        string nombre = textoNombre.text;
+        string ciudad = textoCiudad.text;
+        string mail = textoMail.text;
+        string contrasena = textoContrasena.text;
+        string fechaNaci = textoFechaNaci.text;
+        string nacionalidad = textoNacionalidad.text;
+        //Crear un objeto con los datos
+        WWWForm forma = new WWWForm();
+        forma.AddField("nombre", nombre);
+        forma.AddField("ciudad", ciudad);
+        forma.AddField("mail", mail);
+        forma.AddField("contrasena", contrasena);
+        forma.AddField("fechaNaci", fechaNaci);
+        forma.AddField("nacionalidad", nacionalidad);
+
+        UnityWebRequest request = UnityWebRequest.Post("link de api hecha con lo de marciano", forma);
+        yield return request.SendWebRequest();
+        //....despues de cierto tiempo
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            string texto = request.downloadHandler.text;
+            resultado.text = texto;
+        }
+        else
+        {
+            resultado.text = "Error: " + request.ToString();
+        }
+    }
+}
