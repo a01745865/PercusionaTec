2bb717272b5041acce09f92fe80cdf826df90c39
A01745419 <69483112+A01745419@users.noreply.github.com>
2022-04-21 19:59:24 -0500

    C# login (aun no funciona)
    Como no podemos usar php, habria que hacer api de marciano

--- Assets/Scripts/Login.cs
+++ Assets/Scripts/Login.cs
@@ -3,20 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.Networking;
-// Autores: Jose Luis Madrigal, Erika Marlene Garcia, Cesar Emiliano Palome, Jose Angel Garcia
-// Clase para hacer conexion web con unity para registrar datos
-
-public class RegistrarDatos : MonoBehaviour
+// Autor: Jose Luis Madrigal
+// Clase para hacer conexion web con unity para login
+public class Login : MonoBehaviour
 {
     public TextMeshProUGUI resultado;
 
     //Los datos de entrada
-    public TMP_InputField textoNombre;
-    public TMP_InputField textoCiudad;
-    public TMP_InputField textoMail;
+    public TMP_InputField textoUsuario;
     public TMP_InputField textoContrasena;
-    public TMP_InputField textoFechaNaci;
-    public TMP_InputField textoNacionalidad;
 
     //Enviar los datos al servidor(click del boton)
     public void EnviarDatos()
@@ -27,20 +22,12 @@ public class RegistrarDatos : MonoBehaviour
     private IEnumerator SubirDatos()
     {
         //Recuperar los datos
-        string nombre = textoNombre.text;
-        string ciudad = textoCiudad.text;
-        string mail = textoMail.text;
+        string usuario = textoUsuario.text;
         string contrasena = textoContrasena.text;
-        string fechaNaci = textoFechaNaci.text;
-        string nacionalidad = textoNacionalidad.text;
         //Crear un objeto con los datos
         WWWForm forma = new WWWForm();
-        forma.AddField("nombre", nombre);
-        forma.AddField("ciudad", ciudad);
-        forma.AddField("mail", mail);
+        forma.AddField("usuario", usuario);
         forma.AddField("contrasena", contrasena);
-        forma.AddField("fechaNaci", fechaNaci);
-        forma.AddField("nacionalidad", nacionalidad);
 
         UnityWebRequest request = UnityWebRequest.Post("link de api hecha con lo de marciano", forma);
         yield return request.SendWebRequest();
