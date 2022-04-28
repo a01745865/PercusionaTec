c7c97d98ace95ffa1676f04fd3620cd3e51710d2
a01745865 <a01745865@itesm.mx>
2022-04-28 18:52:56 -0500

    Cambio en cambionivelTAMBOR

--- Assets/Scripts/TAMBOR/cambionivelTAMBOR.cs
+++ Assets/Scripts/TAMBOR/cambionivelTAMBOR.cs
@@ -122,5 +122,7 @@ public class cambionivelTAMBOR : MonoBehaviour
         string URLRegistroIntento = "http://localhost:3000/partida_nivel_intento";
         UnityWebRequest requestRegistroIntento = UnityWebRequest.Post(URLRegistroIntento, formaRegistraIntento);
         yield return requestRegistroIntento.SendWebRequest();
+
+
     }
 }
